a4f33261c47501f04608cdc1ee8f4432de97af4a
bryan_garenne <bryan.garenne.simplon@gmail.com>
2020-03-05 16:22:11 +0100

    clean dev. delete autocomplétion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,7 @@
-//CSS Files importations
-import "../css/style.css"
-
-//JS Files importations
-import $ from "jquery"
-require("foundation-sites")
-import "./autocompletion"
-import "./initmap"
+import "../css/style.css";
+import $ from "jquery";
+require("foundation-sites");
+import "./initmap";
 
 
 
