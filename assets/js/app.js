2f8be9440ae89790015b020712aa5b3ed016b059
Bryan <bgarenne2@gmail.com>
2020-03-07 19:08:08 +0100

    create menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,30 @@
-//CSS Files importations
-import "../css/style.css";
-import ""
-import $ from "jquery";
+import "../scss/style.scss";
 import "foundation-sites/js/foundation";
+import "./autoComplete";
 
 import {addNameWebStorage} from "./webStorage";
 import {initMap, showMarker} from "./initialization";
 
-// import logo from '../image/logo.png';
-
-
+let map = initMap();
 let localStorageAllName = localStorage.getItem('allName');
+
+const menu = document.querySelector('div.menu-app');
+const buttonClose = document.querySelector('.button-close-container');
+const buttonOpen = document.querySelector('.burger-icon-container');
+
 if (!localStorageAllName) {addNameWebStorage();}
 
-let map = initMap();
+
 showMarker(map, 45.188529, 5.724524, 'Chavant');
 
 // console.log(localStorageAllName.split(","));
 // console.log(localStorage);
+
+
+buttonClose.addEventListener('click', event => {
+    menu.style.display = "none";
+});
+
+buttonOpen.addEventListener('click', event => {
+    menu.style.display = "block";
+});
