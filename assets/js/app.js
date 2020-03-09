fc1bc6d5619acb768ccdd4e8ee78d1a4f1ae077f
bryan_garenne <bryan.garenne.simplon@gmail.com>
2020-03-09 15:12:54 +0100

    create function show all marker

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,11 @@
 import "../scss/style.scss";
 import "foundation-sites/js/foundation";
 import "./autoComplete";
-import {initMap, showMarker} from "./initialization";
+import {initMap, showMarker, showAllMarker} from "./initialization";
 import {addCurrentStopLocalStorage, addNameLocalStorage} from "./webStorage";
 
 let map = initMap();
+// showAllMarker(map);
 let localStorageAllName = localStorage.getItem('allName');
 
 if (!localStorageAllName) {addNameLocalStorage();}
@@ -12,7 +13,7 @@ if (!localStorageAllName) {addNameLocalStorage();}
 // console.log(localStorageAllName.split(","));
 // console.log(localStorage);
 
-////////////////////////////////////////////////////////////////////////////////////////// SHOW MENU
+////////////////////////////////////////////////////////////////////////////////////////// SHOW MENU CLICK
 const menu = document.querySelector('div.menu-app');
 const buttonClose = document.querySelector('.button-close-container');
 const buttonOpen = document.querySelector('.burger-icon-container');
