0b40e380711527dc6cf124628ab1b94c5fda2687
bryan_garenne <bryan.garenne.simplon@gmail.com>
2020-03-09 14:31:14 +0100

    merge menu => dev

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,21 @@
 import "../scss/style.scss";
 import "foundation-sites/js/foundation";
 import "./autoComplete";
-
-import {addNameWebStorage} from "./webStorage";
 import {initMap, showMarker} from "./initialization";
-import "foundation-sites/js/foundation";
 import {addCurrentStopLocalStorage, addNameLocalStorage} from "./webStorage";
 
 let map = initMap();
 let localStorageAllName = localStorage.getItem('allName');
 
-const menu = document.querySelector('div.menu-app');
-const buttonClose = document.querySelector('.button-close-container');
-const buttonOpen = document.querySelector('.burger-icon-container');
-
 if (!localStorageAllName) {addNameLocalStorage();}
-
-
-showMarker(map, 45.188529, 5.724524, 'Chavant');
 
 // console.log(localStorageAllName.split(","));
 // console.log(localStorage);
 
+////////////////////////////////////////////////////////////////////////////////////////// SHOW MENU
+const menu = document.querySelector('div.menu-app');
+const buttonClose = document.querySelector('.button-close-container');
+const buttonOpen = document.querySelector('.burger-icon-container');
 
 buttonClose.addEventListener('click', event => {
     menu.style.display = "none";
