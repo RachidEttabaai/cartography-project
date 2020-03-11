a1117271f4818ee93fc01a92ce15b548b4f6915f
bryan_garenne <bryan.garenne.simplon@gmail.com>
2020-03-11 11:13:41 +0100

    error stopListTab note defind

--- assets/js/autoComplete.js
+++ assets/js/autoComplete.js
@@ -3,38 +3,37 @@ import {addCurrentStopLocalStorage} from "./webStorage";
 let allName = localStorage.getItem("allName");
 if (allName) {
     let stopListTab = localStorage.getItem("allName").split(",");
-}
+    $(document).ready(function () {
+        $("#stop-search").on("input", function (e) {
 
+            let valStopSearch = $(this).val();
+            let datalist = $("#search-stop-search");
+            let optionlist = [];
 
-$(document).ready(function () {
-    $("#stop-search").on("input", function (e) {
-
-        let valStopSearch = $(this).val();
-        let datalist = $("#search-stop-search");
-        let optionlist = [];
-
-        if(valStopSearch < 1){
-            return false;
-        }
-
-        datalist.empty();
-
-        stopListTab.forEach(stop => {
-            if(stop.substr(0,valStopSearch.length).toUpperCase() == valStopSearch.toUpperCase()){
-                optionlist.push(stop);
+            if(valStopSearch < 1){
+                return false;
             }
+
+            datalist.empty();
+
+            stopListTab.forEach(stop => {
+                if(stop.substr(0,valStopSearch.length).toUpperCase() == valStopSearch.toUpperCase()){
+                    optionlist.push(stop);
+                }
+            });
+
+            optionlist.forEach(option => {
+                let opt = $("<option></option>").attr("value", option);
+                datalist.append(opt);
+            });
         });
 
-        optionlist.forEach(option => {
-            let opt = $("<option></option>").attr("value", option);
-            datalist.append(opt);
-        });
     });
 
-});
+    $("#search-form").on("submit", function (e) {
+        e.preventDefault();
+        addCurrentStopLocalStorage($("#stop-search").val());
+    });
+}
 
-$("#search-form").on("submit", function (e) {
-    e.preventDefault();
-    addCurrentStopLocalStorage($("#stop-search").val());
-});
 
